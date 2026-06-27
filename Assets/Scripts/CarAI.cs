5745bfb6912852cf2c2b5b89131c534ad32b2514
Tatanagi <kayrafaelchester1925@gmail.com>
2026-06-27 22:54:18 +0800

    UPDATE

--- Assets/Scripts/CarAI.cs
+++ Assets/Scripts/CarAI.cs
@@ -1,70 +1,60 @@
 using UnityEngine;
 using UnityEngine.AI;
-
 [RequireComponent(typeof(NavMeshAgent))]
 public class CarAI : MonoBehaviour
 {
     private NavMeshAgent carAgent;
-
     [Header("Route")]
-    public Transform[] destination;
+    public Transform[] startRoute;
     public int currentWaypointIndex = 0;
-
+    public Transform[] currentRoute;
+    public Transform[] route_1;
+    public Transform[] route_2;
+    public Transform[] route_3;
+    bool routeChosen = false;
     [Header("Traffic Light")]
     public StopLight trafficLight;
     public Transform stopPoint;
     public float stopDistance = 2f;
-
     [Header("Speed")]
     public float normalSpeed = 5f;
     public float slowSpeed = 2f;
     public float dist;
-
     [Header("Raycast Car Detection")]
     public Transform rayOrigin;
     public float rayDistance = 4f;
     public LayerMask carLayer;
     public bool carInFront = false;
-
+    //public float dist;
     private void Start()
     {
         carAgent = GetComponent<NavMeshAgent>();
         carAgent.speed = normalSpeed;
-
-        if (destination.Length > 0)
-        {
-            MoveToCurrentWaypoint();
-        }
+        currentRoute = startRoute;
+        currentWaypointIndex = 0;
+        MoveToCurrentWaypoint();
     }
-
     private void Update()
     {
         CheckCarInFront();
-
         if (carInFront)
         {
             carAgent.isStopped = true;
             return;
         }
-
         FollowTrafficLight();
-
         if (carAgent.pathPending)
             return;
-
         if (carAgent.remainingDistance <= carAgent.stoppingDistance)
         {
             GoToNextWaypoint();
         }
     }
-
     private void FollowTrafficLight()
     {
         if (trafficLight == null || stopPoint == null)
             return;
-
         dist = Vector3.Distance(transform.position, stopPoint.position);
-
         // Stop at red light
         if (!trafficLight.isGreen && dist <= stopDistance)
         {
@@ -77,41 +67,57 @@ public class CarAI : MonoBehaviour
             carAgent.speed = normalSpeed;
         }
     }
-
     private void MoveToCurrentWaypoint()
     {
-        if (destination.Length == 0)
+        if (currentRoute.Length == 0 || currentRoute == null)
             return;
-
-        carAgent.SetDestination(destination[currentWaypointIndex].position);
+        carAgent.SetDestination(currentRoute[currentWaypointIndex].position);
     }
-
     private void GoToNextWaypoint()
     {
         currentWaypointIndex++;
-
-        if (currentWaypointIndex >= destination.Length)
+        if (currentWaypointIndex >= currentRoute.Length)
         {
-            currentWaypointIndex = 0;
+            if (!routeChosen)
+            {
+                ChooseRandomRoute();
+            }
+            else
+            {
+                currentWaypointIndex = 0;
+                MoveToCurrentWaypoint();
+            }
         }
-
         MoveToCurrentWaypoint();
     }
-
+    public void ChooseRandomRoute()
+    {
+        routeChosen = true;
+        int randomRoute = Random.Range(0, 2);
+        if (randomRoute == 0)
+        {
+            currentRoute = route_1;
+        }
+        else if (randomRoute == 1)
+        {
+            currentRoute = route_2;
+        }
+        else
+        {
+            currentRoute = route_3;
+        }
+        currentWaypointIndex = 0;
+        MoveToCurrentWaypoint();
+    }
     private void CheckCarInFront()
     {
         carInFront = false;
-
         if (rayOrigin == null)
             return;
-
         Vector3 origin = rayOrigin.position;
         Vector3 direction = transform.forward;
-
         Debug.DrawRay(origin, direction * rayDistance, Color.yellow);
-
         RaycastHit hit;
-
         if (Physics.Raycast(origin, direction, out hit, rayDistance, carLayer))
         {
             carInFront = true;
